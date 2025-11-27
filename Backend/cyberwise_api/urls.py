e662e591af7ba83d29b55475b9335e6b530f53a5
Eduardo Melo <eduardom.dsilva7@gmail.com>
2025-11-27 01:15:31 -0300

    Fix: Forçando Django a servir media files em produção

--- Backend/cyberwise_api/urls.py
+++ Backend/cyberwise_api/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
